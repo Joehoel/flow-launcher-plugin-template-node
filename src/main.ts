de4f9ff6f1674629e44b9fd36ef6e33f39080929
Joël Kuijper <jmkuijper@outlook.com>
2023-02-18 12:48:08 +0100

    feat: add lint-staged

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,34 @@ import { z } from "zod";
 
 // The events are the custom events that you define in the flow.on() method.
 const events = ["search"] as const;
-type Events = typeof events[number];
+type Events = (typeof events)[number];
 
 const flow = new Flow<Events>("assets/npm.png");
 
-flow.on("query", params => {
-  const [query] = z.array(z.string()).parse(params);
+flow.on("query", (params) => {
+	const [query] = z.array(z.string()).parse(params);
 
-  const qp = new URLSearchParams({
-    q: query,
-  });
+	const qp = new URLSearchParams({
+		q: query,
+	});
 
-  const url = `https://www.npmjs.com/search?${qp}`;
+	const url = `https://www.npmjs.com/search?${qp}`;
 
-  flow.showResult({
-    title: `Search NPM package: ${query}`,
-    subtitle: url,
-    method: "search",
-    parameters: [url],
-    dontHideAfterAction: true,
-  });
+	flow.showResult({
+		title: `Search NPM package: ${query}`,
+		subtitle: url,
+		method: "search",
+		parameters: [url],
+		dontHideAfterAction: true,
+	});
 });
 
-flow.on("search", params => {
-  const [url] = z.array(z.string().url()).parse(params);
+flow.on("search", (params) => {
+	const [url] = z.array(z.string().url()).parse(params);
 
-  flow.shellRun({ command: "open ." });
+	flow.shellRun({ command: "open ." });
 
-  open(url);
+	open(url);
 });
 
 flow.run();
